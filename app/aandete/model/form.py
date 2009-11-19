3e43656167a4fdf160356059182213ccbf121e15
Stefan van der Walt <stefan@sun.ac.za>
2009-11-19 17:24:55 +0200

    Do not filter extra fields.

--- app/aandete/model/form.py
+++ app/aandete/model/form.py
@@ -5,5 +5,5 @@ import formencode
 
 class RecipeForm(formencode.Schema):
     allow_extra_fields = True
-    filter_extra_fields = True
+    filter_extra_fields = False
     title = formencode.validators.String(not_empty=True)
